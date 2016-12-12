25e224d95ee6e6d08dcbafb5f0a52f360d15dec6
Yevgeniy Poluektov <evgeniy.v.poluektov@gmail.com>
2016-12-13 00:06:05 +0300

    finaly resolve nginx spring boot issue

--- commands.sh
+++ commands.sh
@@ -1,2 +1,10 @@
 APP_ENV=production SSH_USER=jeck SERVER_IP=jeckep.online ./deploy.sh  -c
 APP_ENV=production SSH_USER=jeck SERVER_IP=jeckep.online ./deploy.sh  -b
+
+
+docker run -it --rm --name my-nginx \
+  -p 80:80 -p 443:443 \
+  -v /Users/jeckep/projects/my/deploy/env/production/certs:/etc/ssl/certs \
+  -v /Users/jeckep/projects/my/deploy/nginx/configs:/configs \
+  my-nginx
+
